96dba7519ca5ddd40bfd6a9d0cf8c9ee58475a7f
Magisk317 <magisk317@users.noreply.github.com>
2026-02-14 10:22:50 +0800

    build(apk): enable abi splits by default

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,14 +75,12 @@ android {
         buildConfigField("int", "MODULE_VERSION", "$versionCodeInt")
     }
 
-    if (project.hasProperty("buildSplits")) {
-        splits {
-            abi {
-                isEnable = true
-                reset()
-                include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-                isUniversalApk = true
-            }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
         }
     }
 
