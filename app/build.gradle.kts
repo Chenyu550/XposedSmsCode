b344f10476e18b6fae5bf0970311ee93bbf64887
Magisk317 <magisk317@users.noreply.github.com>
2026-02-15 09:14:33 +0800

    fix(build): disable abi splits by default to fix AAB build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ android {
 
     splits {
         abi {
-            isEnable = true
+            isEnable = hasProperty("buildSplits")
             reset()
             include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
             isUniversalApk = true
