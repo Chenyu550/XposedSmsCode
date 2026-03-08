35bc80bff2a41f381f6785c700b8276df3781a80
Magisk317 <magisk317@users.noreply.github.com>
2026-03-08 19:43:07 +0800

    docs(readme): remove Xposed API badge and simplify badge sync

--- scripts/sync_readme_badges.sh
+++ scripts/sync_readme_badges.sh
@@ -29,13 +29,12 @@ COMPOSE_BOM_VERSION=$(read_version "compose-bom")
 AGP_VERSION=$(read_version "agp")
 MIN_SDK_VERSION=$(read_version "minSdk")
 TARGET_SDK_VERSION=$(read_version "targetSdk")
-XPOSED_API_VERSION=$(read_version "xposed")
 
 KOTLIN_BADGE=$(badge_escape "$KOTLIN_VERSION")
 COMPOSE_BADGE=$(badge_escape "$COMPOSE_BOM_VERSION")
 AGP_BADGE=$(badge_escape "$AGP_VERSION")
 
-SECOND_BADGE_LINE="[![Kotlin](https://img.shields.io/badge/Kotlin-${KOTLIN_BADGE}-7F52FF?style=flat-square&logo=kotlin&logoColor=white)](https://kotlinlang.org) [![Jetpack Compose](https://img.shields.io/badge/Jetpack_Compose-BOM_${COMPOSE_BADGE}-4285F4?style=flat-square&logo=android&logoColor=white)](https://developer.android.com/jetpack/compose) [![Gradle](https://img.shields.io/badge/Gradle-9.5.0--nightly-02303A?style=flat-square&logo=gradle&logoColor=white)](https://gradle.org) [![AGP](https://img.shields.io/badge/AGP-${AGP_BADGE}-3DDC84?style=flat-square&logo=gradle&logoColor=white)](https://developer.android.com/studio/releases/gradle-plugin) [![Min SDK](https://img.shields.io/badge/Min_SDK-${MIN_SDK_VERSION}-brightgreen?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Target SDK](https://img.shields.io/badge/Target_SDK-${TARGET_SDK_VERSION}-blue?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Xposed API](https://img.shields.io/badge/Xposed_API-${XPOSED_API_VERSION}-orange?style=flat-square)](https://github.com/rovo89/XposedBridge) [![Telegram](https://img.shields.io/badge/Telegram-Group-2CA5E0?style=flat-square&logo=telegram&logoColor=white)](https://t.me/+NR2QaQ4dlEgxYmNl)"
+SECOND_BADGE_LINE="[![Kotlin](https://img.shields.io/badge/Kotlin-${KOTLIN_BADGE}-7F52FF?style=flat-square&logo=kotlin&logoColor=white)](https://kotlinlang.org) [![Jetpack Compose](https://img.shields.io/badge/Jetpack_Compose-BOM_${COMPOSE_BADGE}-4285F4?style=flat-square&logo=android&logoColor=white)](https://developer.android.com/jetpack/compose) [![Gradle](https://img.shields.io/badge/Gradle-9.5.0--nightly-02303A?style=flat-square&logo=gradle&logoColor=white)](https://gradle.org) [![AGP](https://img.shields.io/badge/AGP-${AGP_BADGE}-3DDC84?style=flat-square&logo=gradle&logoColor=white)](https://developer.android.com/studio/releases/gradle-plugin) [![Min SDK](https://img.shields.io/badge/Min_SDK-${MIN_SDK_VERSION}-brightgreen?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Target SDK](https://img.shields.io/badge/Target_SDK-${TARGET_SDK_VERSION}-blue?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Telegram](https://img.shields.io/badge/Telegram-Group-2CA5E0?style=flat-square&logo=telegram&logoColor=white)](https://t.me/+NR2QaQ4dlEgxYmNl)"
 
 for readme in README.md README-EN.md; do
   tmp_file="$(mktemp)"
