7ee785d4f3aa3a93e3eeefbc59e474ad24f8bb15
Magisk317 <magisk317@users.noreply.github.com>
2026-03-26 10:37:44 +0800

    build: add magisk-ui-kit as submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,3 @@ pluginManagement {
 }
 
 include(":app", ":storage", ":core", ":smscode-core:smscode-xposed-core", ":smscode-core:smscode-domain", ":xposed-stub", ":magisk-ui-kit")
-project(":magisk-ui-kit").projectDir = file("../magisk-ui-kit")
