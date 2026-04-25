ec37ba6de9e213df5b386a29c1bbbefb32bf919e
magisk317 <93979778+magisk317@users.noreply.github.com>
2026-04-25 16:45:12 +0800

    chore(deps): migrate dependency automation to self-hosted renovate

---
+++ .github/renovate-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  platform: "github",
+  repositories: ["magisk317/XposedSmsCode"],
+  onboarding: false,
+  requireConfig: "required",
+};
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,39 +3,25 @@ import com.adarshr.gradle.testlogger.theme.ThemeType
 import org.gradle.api.tasks.Exec
 
 buildscript {
-    val nettyVersion = "4.1.129.Final"
-    val nettyModules = listOf(
-        "netty-codec",
-        "netty-codec-http",
-        "netty-codec-http2",
-        "netty-common",
-        "netty-handler",
-    )
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
-    dependencies {
-        constraints {
-            nettyModules.forEach { module ->
-                classpath("io.netty:$module:$nettyVersion") {
-                    because("Dependabot: declare patched netty modules for build/plugin dependency graph")
-                }
-            }
-        }
-    }
     configurations.all {
         resolutionStrategy {
-            force(libs.jose4j)
-            force(libs.jdom2)
-            force(libs.apache.commons.lang3)
-            eachDependency {
-                if (requested.group == "io.netty") {
-                    useVersion(nettyVersion)
-                    because("Dependabot: netty CVE fixes (transitive build/test deps)")
-                }
-            }
+            // BEGIN AUTO FORCED DEPENDENCIES (managed by workflow)
+            force("io.netty:netty-codec:4.1.125.Final")
+            force("io.netty:netty-codec-http:4.1.132.Final")
+            force("io.netty:netty-codec-http2:4.1.132.Final")
+            force("io.netty:netty-common:4.1.118.Final")
+            force("io.netty:netty-handler:4.1.118.Final")
+            force("org.apache.commons:commons-lang3:3.20.0")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.bouncycastle:bcpkix-jdk18on:1.84")
+            force("org.bouncycastle:bcprov-jdk18on:1.84")
+            force("org.jdom:jdom2:2.0.6.1")
+            // END AUTO FORCED DEPENDENCIES (managed by workflow)
         }
     }
 }
@@ -130,11 +116,18 @@ subprojects {
 
     configurations.all {
         resolutionStrategy {
-            force(catalog.jose4j)
-            force(catalog.jdom2)
-            force(catalog.apache.commons.lang3)
             force(catalog.apache.httpclient)
             // BEGIN AUTO FORCED DEPENDENCIES (managed by workflow)
+            force("io.netty:netty-codec:4.1.125.Final")
+            force("io.netty:netty-codec-http:4.1.132.Final")
+            force("io.netty:netty-codec-http2:4.1.132.Final")
+            force("io.netty:netty-common:4.1.118.Final")
+            force("io.netty:netty-handler:4.1.118.Final")
+            force("org.apache.commons:commons-lang3:3.20.0")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.bouncycastle:bcpkix-jdk18on:1.84")
+            force("org.bouncycastle:bcprov-jdk18on:1.84")
+            force("org.jdom:jdom2:2.0.6.1")
             // END AUTO FORCED DEPENDENCIES (managed by workflow)
         }
     }
