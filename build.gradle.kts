81c74e6a3f946b981c331ce91d4072c2c11e6cb9
magisk317 <93979778+magisk317@users.noreply.github.com>
2026-04-28 05:54:50 +0000

    build: update forced dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,6 @@ buildscript {
     configurations.all {
         resolutionStrategy {
             // BEGIN AUTO FORCED DEPENDENCIES (managed by workflow)
-            force("io.netty:netty-codec:4.2.12.Final")
-            force("io.netty:netty-codec-http:4.2.12.Final")
-            force("io.netty:netty-codec-http2:4.2.12.Final")
-            force("io.netty:netty-common:4.2.12.Final")
-            force("io.netty:netty-handler:4.2.12.Final")
             force("org.apache.commons:commons-lang3:3.20.0")
             force("org.bitbucket.b_c:jose4j:0.9.6")
             force("org.bouncycastle:bcpkix-jdk18on:1.84")
@@ -118,11 +113,6 @@ subprojects {
         resolutionStrategy {
             force(catalog.apache.httpclient)
             // BEGIN AUTO FORCED DEPENDENCIES (managed by workflow)
-            force("io.netty:netty-codec:4.2.12.Final")
-            force("io.netty:netty-codec-http:4.2.12.Final")
-            force("io.netty:netty-codec-http2:4.2.12.Final")
-            force("io.netty:netty-common:4.2.12.Final")
-            force("io.netty:netty-handler:4.2.12.Final")
             force("org.apache.commons:commons-lang3:3.20.0")
             force("org.bitbucket.b_c:jose4j:0.9.6")
             force("org.bouncycastle:bcpkix-jdk18on:1.84")
