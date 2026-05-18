232586a04f3c3cde11dc6046b6ac9d552202a1f8
magisk317 <93979778+magisk317@users.noreply.github.com>
2026-05-18 11:22:39 +0800

    ci: fix renovate flow - allow gradle wrapper and recreate blocked branches
    - Add allowedUnsafeExecutions for gradleWrapper (aligns with sibling projects)
    - Change schedule from every 6h to daily at 00:00 UTC (08:00 Beijing time)
    - Add recreateWhen: always to unblock edited renovate/all-minor-patch branch

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -3,5 +3,6 @@ module.exports = {
   repositories: ["magisk317/XposedSmsCode"],
   onboarding: false,
   requireConfig: "required",
+  allowedUnsafeExecutions: ["gradleWrapper"],
   gitAuthor: "magisk317 <93979778+magisk317@users.noreply.github.com>",
 };
